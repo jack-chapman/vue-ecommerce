ad759051cf0fcd67dcf48fae6057ed0d546a6bf2
jack-chapman <jackchapman093@gmail.com>
2021-07-08 21:07:20 +0100

    Add icons and component auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
+import Icons, { ViteIconsResolver } from 'vite-plugin-icons';
+import Components from 'vite-plugin-components';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(),
+    WindiCSS(),
+    Components({
+      customComponentResolvers: ViteIconsResolver(),
+      globalComponentsDeclaration: true,
+    }),
+    Icons(),
+  ],
 });
